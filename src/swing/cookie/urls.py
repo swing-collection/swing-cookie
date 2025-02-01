9076aba1c3d419e465a30c38ce2a9689c5827a61
Lars van Vianen <lars@vianen.com>
2025-02-01 15:00:13 +0100

    Update urls.py

--- src/swing/cookie/urls.py
+++ src/swing/cookie/urls.py
@@ -1,53 +1,77 @@
 # -*- coding: utf-8 -*-
 
-from django.urls import path
 
-from .views import delete_cookie_view, get_cookie_view, set_cookie_view
+# =============================================================================
+# Docstring
+# =============================================================================
 
-urlpatterns = [
-    path("set-cookie/", set_cookie_view, name="set_cookie_view"),
-    path("get-cookie/", get_cookie_view, name="get_cookie_view"),
-    path("delete-cookie/", delete_cookie_view, name="delete_cookie_view"),
-]
+"""
+Cookie Consent URL Configuration
+================================
+
+This module defines URL patterns for managing cookies, including setting,
+getting, and deleting cookies, as well as handling user consent.
+
+"""
 
 
+# =============================================================================
+# Imports
+# =============================================================================
+
+# Import | Django Libraries
 from django.urls import path, re_path
 from django.views.decorators.csrf import csrf_exempt
 
+# Import | Local Views
 from .views import (
     CookieGroupAcceptView,
     CookieGroupDeclineView,
     CookieGroupListView,
     CookieStatusView,
+    delete_cookie_view,
+    get_cookie_view,
+    set_cookie_view,
 )
 
-urlpatterns = [
+# =============================================================================
+# URL Patterns
+# =============================================================================
+
+cookie_management_patterns = [
+    path("set-cookie/", set_cookie_view, name="set_cookie_view"),
+    path("get-cookie/", get_cookie_view, name="get_cookie_view"),
+    path("delete-cookie/", delete_cookie_view, name="delete_cookie_view"),
+]
+
+cookie_consent_patterns = [
     path(
-        "accept/",
-        csrf_exempt(CookieGroupAcceptView.as_view()),
+        route="accept/",
+        view=csrf_exempt(CookieGroupAcceptView.as_view()),
         name="cookie_consent_accept_all",
     ),
-    # TODO: use form or query string params for this instead?
     re_path(
-        r"^accept/(?P<varname>.*)/$",
-        csrf_exempt(CookieGroupAcceptView.as_view()),
+        route=r"^accept/(?P<varname>[\w-]+)/$",
+        view=csrf_exempt(CookieGroupAcceptView.as_view()),
         name="cookie_consent_accept",
     ),
-    # TODO: use form or query string params for this instead?
     re_path(
-        r"^decline/(?P<varname>.*)/$",
-        csrf_exempt(CookieGroupDeclineView.as_view()),
+        route=r"^decline/(?P<varname>[\w-]+)/$",
+        view=csrf_exempt(CookieGroupDeclineView.as_view()),
         name="cookie_consent_decline",
     ),
     path(
-        "decline/",
-        csrf_exempt(CookieGroupDeclineView.as_view()),
+        route="decline/",
+        view=csrf_exempt(CookieGroupDeclineView.as_view()),
         name="cookie_consent_decline_all",
     ),
     path("status/", CookieStatusView.as_view(), name="cookie_consent_status"),
     path(
-        "",
-        CookieGroupListView.as_view(),
+        route="",
+        view=CookieGroupListView.as_view(),
         name="cookie_consent_cookie_group_list",
     ),
 ]
+
+# Consolidate all URL patterns
+urlpatterns = cookie_management_patterns + cookie_consent_patterns
